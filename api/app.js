4eb8ca446a8b1960fdd1fdca964d5306790f08e9
Niamh <niamh@Niamhs-MacBook-Pro.local>
2022-09-06 10:42:34 +0100

    implemented 5-get-users

--- api/app.js
+++ api/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const { getTopics } = require('./controllers/topics.controllers')
 const { handleCustomErrors, handleInternalServerErrors, allMethodErrors, handlePSQLErrors } = require('./controllers/errors.controllers')
 const { getArticleById } = require('./controllers/articles.controllers')
+const { getUsers } = require('./controllers/users.controllers')
 const app = express();
 
 
 app.get('/api/topics', getTopics);
 app.get(`/api/articles/:article_id`, getArticleById);
+app.get(`/api/users`, getUsers)
 
 
 
