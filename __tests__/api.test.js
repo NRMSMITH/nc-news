3d9f3b15a07494cde53c3369fc7f80fba2a70547
NRMSMITH <niamhrmsmith94@hotmail.co.uk>
2022-09-28 12:08:37 +0100

    updated tests

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -452,7 +452,7 @@ describe('POST: /api/articles/:article_id/comments', () => {
         });
 })
 
-describe.only('DELETE: /api/comments/:comment_id', () => {
+describe('DELETE: /api/comments/:comment_id', () => {
     test('204: should return no content but delete a comment', () => {
         return request(app)
         .delete('/api/comments/1')
